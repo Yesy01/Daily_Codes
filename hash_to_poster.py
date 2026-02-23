bbee71a32ab2ff1e706f78c6e893844fbcf3ede3
Yesirat <yesiratsanni49@gmail.com>
2026-02-24 02:30:52 +0530

    Doc: Added hash_to_poster.py, this script converts
    a hash value to a poster image. It uses the hash value to generate a
    unique poster design SVG format,
     which can be used for visual representations of data. The script is
     designed to be simple and efficient, allowing for quick generation of
     posters based on hash values.

---
+++ hash_to_poster.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import argparse
+import hashlib
+import math
+from pathlib import Path
+from typing import Dict, List, Tuple
+
+
+MASK32 = 0xFFFFFFFF
+U32_DEN = 4294967296.0
